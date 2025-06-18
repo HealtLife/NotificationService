a3c58ca3707a3a9174a4577da5d22abb480d43cc
joseluza <u202213404@upc.edu.pe>
2025-06-18 01:26:18 -0500

    Update de Propiedades

--- src/main/java/com/acme/nutrimove/notificationservice/NotificationServiceApplication.java
+++ src/main/java/com/acme/nutrimove/notificationservice/NotificationServiceApplication.java
@@ -2,10 +2,10 @@ package com.acme.nutrimove.notificationservice;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
-import org.springframework.cloud.netflix.eureka.EnableEurekaClient;
+import org.springframework.data.jpa.repository.config.EnableJpaAuditing;
 
 @SpringBootApplication
-@EnableEurekaClient
+@EnableJpaAuditing
 public class NotificationServiceApplication {
     public static void main(String[] args) {
         SpringApplication.run(NotificationServiceApplication.class, args);
